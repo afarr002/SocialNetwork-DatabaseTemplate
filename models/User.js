015638a11c0bbff2cee8098ab89756a417346614
aaron <afarrell002@gmail.com>
2022-01-12 15:42:22 -0700

    added regex validators for both user email and user username on user.js model

--- models/User.js
+++ models/User.js
@@ -9,11 +9,14 @@ const userSchema = new Schema(
       required: true,
       trim: true,
       max_length: 25,
+      validate: /^[a-zA-Z0-9_-]{1,25}$/,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      validate:
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     },
     thoughts: [thoughtSchema],
     friends: [userSchema],
