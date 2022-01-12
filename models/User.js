59091397d2866486265b4af73efab80a0fb26256
aaron <afarrell002@gmail.com>
2022-01-12 11:11:14 -0700

    created basic structure for User model, need to update email validation and create virtual called friendCount that retreives the length of users friends array.

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      max_length: 25,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: [thoughtSchema],
+    friends: [friendCount],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("users", userSchema);
+
+module.exports = User;
