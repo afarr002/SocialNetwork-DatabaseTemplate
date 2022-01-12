f089ecdd7a7617ef09e02ea6af83130982fa49d1
aaron <afarrell002@gmail.com>
2022-01-12 12:39:57 -0700

    created basic structure for models/thought.js, required in reaction schema for use on reactions key

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,34 @@
 const { Schema, Types } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
-  thoughtId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const thoughtSchema = new Schema(
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+module.exports = thoughtSchema;
