64f246c5c8f97ff7118c9c8d926e7af18410bfae
aaron <afarrell002@gmail.com>
2022-01-12 12:34:14 -0700

    set virtuals to true on models/user.js

---
+++ models/Thought.js
@@ -0,0 +1,18 @@
+const { Schema, Types } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
--- models/User.js
+++ models/User.js
@@ -21,6 +21,7 @@ const userSchema = new Schema(
   {
     toJSON: {
       getters: true,
+      virtuals: true,
     },
   }
 );
