53fdcbbc66af9c671190213ef059f277853dfe45
aaron <afarrell002@gmail.com>
2022-01-13 09:54:03 -0700

    updated thoughts and friends values on User model

--- models/User.js
+++ models/User.js
@@ -18,8 +18,18 @@ const userSchema = new Schema(
       validate:
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     },
-    thoughts: [thoughtSchema],
-    friends: [this],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: {
