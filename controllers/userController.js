0e65e271a3e85458ed9b3a033eefb781792271ff
aaron <afarrell002@gmail.com>
2022-01-12 18:00:20 -0700

    added logic for updateUser to controllers/userController.js and added put route for updateUser to routes/api/usersRoutes.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,4 +61,18 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    ).then((user) =>
+      !user
+        ? res.status(404).json({
+            message: `No user found with this ID!`,
+          })
+        : res.json(user)
+    );
+  },
 };
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -3,11 +3,12 @@ const {
   getSingleUser,
   createUser,
   deleteUser,
+  updateUser,
 } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(User.find()).post(createUser);
 
-router.route("/:userId").get(getSingleUser).delete(deleteUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
