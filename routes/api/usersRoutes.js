3f59e495447b8cd435256ae0f671612e070fd654
aaron <afarrell002@gmail.com>
2022-01-12 18:10:18 -0700

    added logic for addFriend (adds friends to users) to controllers/userController.js and added post route for addFriend to routes/api/usersRoutes.js

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -4,11 +4,16 @@ const {
   createUser,
   deleteUser,
   updateUser,
+  addFriend,
 } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(User.find()).post(createUser);
 
+// /api/users/:userId
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend);
 
 module.exports = router;
