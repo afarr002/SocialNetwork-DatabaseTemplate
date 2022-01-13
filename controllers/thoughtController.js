6785741e1706f057fb5a39a3ceb76e21ad786cfd
aaron <afarrell002@gmail.com>
2022-01-12 19:11:33 -0700

    added logic for createThought (creates new thought) to controllers/thoughtController.js and added post route for createThought to routes/api/thoughtsRoutes.js - still ned to update to push the new thought's _id to the assocaited user's thoughts array

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -20,4 +20,13 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
 };
