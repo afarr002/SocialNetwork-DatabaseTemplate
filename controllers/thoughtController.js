30e36db3a91ddd44fa00476583bba3b985c79deb
aaron <afarrell002@gmail.com>
2022-01-12 19:23:56 -0700

    added logic for deleteThought (deletes a thought) to controllers/thoughtController.js and added delete route for deleteThought to routes/api/thoughtsRoutes.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought, User } = require("../models");
+const { Thought, User, Reaction } = require("../models");
 
 module.exports = {
   getThoughts(req, res) {
@@ -28,5 +28,27 @@ module.exports = {
         console.log(err);
         return res.status(500).json(err);
       });
+  },
+
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              messgae: `Hmmm, I wonder.
+              There doesn't seem to be any thought associated with that ID!`,
+            })
+          : Reaction.deleteMany({
+              _id: {
+                $in: thought.reactions,
+              },
+            })
+      )
+      .then(() =>
+        res.json({
+          message: `Thoughts and reactions deleted!`,
+        })
+      )
+      .catch((err) => res.s);
   },
 };
