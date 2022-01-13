71682ff8182e719147bf782a948b5580820fa669
aaron <afarrell002@gmail.com>
2022-01-12 18:56:24 -0700

    added logic for getSingleThought (returns single thought) to controllers/thoughtController.js and added get route for getSingleThought to routes/api/thoughtsRoutes.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,4 +6,18 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: `Hmmmm, curious.
+      No thought with that ID was found!`,
+            })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
